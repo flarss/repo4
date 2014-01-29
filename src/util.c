9cd6af11a9358dbc4c3dd8bc86a5419856d5d07f
Fredrik Larsson <flarss@hotmail.com>
2014-01-29 09:18:38 +0100

    util file per task 16330

---
+++ src/util.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+#ifndef NO_FCNTL_H
+#  include <fcntl.h>
+#endif
+
+#if defined(STDC_HEADERS) || !defined(NO_STDLIB_H)
+#  include <stdlib.h>
+#else
+   extern int errno;
+#endif
+
+/* ========================================================================
+ * Put string s in lower case, return s.
+ */
+char *strlwr(s)
+    char *s;
+{
+    char *t;
+    for (t = s; *t; t++) *t = tolow(*t);
+    return s;
+}
+
+
