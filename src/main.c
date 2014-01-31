0fe85c488c915494f3eddf6638653ec065de746f
Fredrik Larsson <flarss@hotmail.com>
2014-01-31 21:55:36 +0100

    mac was here

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 extern void util();
 int main( int argc, const char* argv[] )
 {
-	printf( "\nHello World, feature_y, enhanced ubuntu\n\n" );
+	printf( "Hello World, feature_y, enhanced ubuntu and mac\n" );
 	util();		
 }
 
