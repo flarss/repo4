0c3fa378ca6043ce7fb23f4eee5c8b717548e51f
Fredrik Larsson <flarss@hotmail.com>
2014-01-31 22:08:11 +0100

    addda

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 extern void util();
 int main( int argc, const char* argv[] )
 {
-	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, hmmmm\n" );
+	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, hmmmm, mer och mer\n" );
 	util();		
 }
 
