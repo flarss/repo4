7c3f50ceb3610e57328ef42c35ee800721241fa7
Fredrik Larsson <flarss@hotmail.com>
2014-01-31 22:05:41 +0100

    add

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 extern void util();
 int main( int argc, const char* argv[] )
 {
-	printf( "Hello World, feature_x, feature_y, enhanced ubuntu\n" );
+	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, hmmmm\n" );
 	util();		
 }
 
