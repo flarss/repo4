60994e5e76060c2139c0fd58514adb24cdecbb71
Fredrik Larsson <flarss@hotmail.com>
2014-02-01 00:06:34 +0100

    fixes task 16588

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@ extern void util();
 int main( int argc, const char* argv[] )
 {
 	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, featZ\n" );
-	printf("ok!\n");
+	printf("Ok!\n");
 	util();		
 }
 
