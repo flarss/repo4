5f8f24bf5fb9c66e1537f6b6d1c42e453d7c5d27
Fredrik Larsson <fredrikgit@gmail.com>
2014-02-10 19:04:49 +0100

    build with more files task 18116

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 extern void util();
+extern void foo();
 int main( int argc, const char* argv[] )
 {
 	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, featZ\n" );
-	printf("Ok!\n");
-	util();		
+	util();	
+	foo();	
 }
 
