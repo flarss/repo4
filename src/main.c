5b7b12602b076dd983ce6fe191be563fdd91170d
Fredrik Larsson <fredrikgit@gmail.com>
2014-03-03 23:19:19 +0100

    featabc

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@ extern void util();
 int main( int argc, const char* argv[] )
 {
 	printf( "Hello World, feature_x, feature_y, enhanced ubuntu, featZ\n" );
-	printf( "mainfeatAT\n")
+	printf( "mainfeatAT\n");
+	printf( "featABC");
 	printf("ok");
 	printf("main")
 	util();	
